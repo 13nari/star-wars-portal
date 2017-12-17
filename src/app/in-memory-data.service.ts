947e53f144ffec36417c9dd8b9e1ae9df11aed73
Rodrigo <rodrigo13nari@gmail.com>
2017-12-17 19:17:58 -0200

    Name changes in mock json

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,28 +3,22 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const characters = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: 'Luke Skywalker' },
+      { id: 12, name: 'C-3PO' },
+      { id: 13, name: 'R2-D2' },
+      { id: 14, name: 'Darth Vader' },
+      { id: 15, name: 'Leia Organa' },
+      { id: 16, name: 'Owen Lars' },
+      { id: 17, name: 'Beru Whitesun lars' },
+      { id: 18, name: 'R5-D4' },
+      { id: 19, name: 'Biggs Darklighter' },
+      { id: 20, name: 'Obi-Wan Kenobi' }
     ];
         const starships = [
-      { id: 21, name: 'Nave 1' },
-      { id: 22, name: 'Nave 2' },
-      { id: 23, name: 'Nave 3' },
-      { id: 24, name: 'Nave 4' },
-      { id: 25, name: 'Nave 5' },
-      { id: 26, name: 'Nave 6' },
-      { id: 27, name: 'Nave 7' },
-      { id: 28, name: 'Nave 8' },
-      { id: 29, name: 'Nave 9' },
-      { id: 30, name: 'Nave 10' }
+      { id: 21, name: 'CR90 corvette' },
+      { id: 22, name: 'Star Destroyer' },
+      { id: 23, name: 'Sentinel-class landing craft' },
+      { id: 24, name: 'Death Star' }
     ];
     return {characters,starships};
   }
